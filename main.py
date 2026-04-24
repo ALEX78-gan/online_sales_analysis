b05c84ff6f817a866ed04d0e3cbfc8b9c561654e
Alexei Gangan <alexgold8438@gmail.com>
2026-04-24 13:13:38 +0300

    Stage 7 changes

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ from product_manager import ProductManager
 
 pm = ProductManager()
 
-p1 = Product("Laptop", 1000, 5)
-p2 = Product("Telefon", 500, 10)
-p3 = Product("Casti", 100, 20)
+p1 = Product("Tv", 2000, 5)
+
+p3 = Product("Mouse", 200, 20)
 
 pm.add_product(p1)
-pm.add_product(p2)
+
 pm.add_product(p3)
 
-pm.display_products()
-print("Valoare totala:", pm.total_value())
